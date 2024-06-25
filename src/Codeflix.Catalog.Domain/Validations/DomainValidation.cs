aaa1a80903a8b4a5a959aa53c180e6acc4ab9488
Marcos Sousa <marcosp.sousa@gmail.com>
2024-06-25 19:53:28 -0300

    Add new validation methods and respective unit tests
    The project has been updated with new validation methods in the DomainValidation class, which check for minimum string length and if a string is not null or empty. In addition, corresponding unit tests have been added to verify these new validation methods. Some other new files related to the project settings have also been added.

--- src/Codeflix.Catalog.Domain/Validations/DomainValidation.cs
+++ src/Codeflix.Catalog.Domain/Validations/DomainValidation.cs
@@ -9,4 +9,17 @@ public class DomainValidation
         if (target == null)
             throw new EntityValidationException($"{fieldName} should not be null");
     }
+    
+    public static void NotNullOrEmpty(string? target, string fieldName)
+    {
+        if (String.IsNullOrWhiteSpace(target))
+            throw new EntityValidationException($"{fieldName} should not be null or empty");
+    }
+    
+    public static void MinLength(string target, int minLength, string fieldName)
+    {
+        if (target.Length < minLength)
+            throw new EntityValidationException($"{fieldName} should be at least {minLength} characters long");
+    }
+    
 }
--- test/Codeflix.Catalog.UnitTest/Domain/Validations/DomainValidationTest.cs
+++ test/Codeflix.Catalog.UnitTest/Domain/Validations/DomainValidationTest.cs
@@ -9,7 +9,7 @@ namespace Codeflix.Catalog.UnitTest.Domain.Validations;
 public class DomainValidationTest
 {
     private Faker Faker { get; set; } = new Faker();
-    
+
     [Fact(DisplayName = nameof(NotNullOk))]
     public void NotNullOk()
     {
@@ -17,12 +17,70 @@ public class DomainValidationTest
         Action action = () => DomainValidation.NotNull(value, "Value");
         action.Should().NotThrow();
     }
-    
+
     [Fact(DisplayName = nameof(NotNullError))]
     public void NotNullError()
     {
         string? value = null;
         Action action = () => DomainValidation.NotNull(value, "FieldName");
         action.Should().Throw<EntityValidationException>().WithMessage("FieldName should not be null");
+    }
+
+    [Theory(DisplayName = nameof(NotNullOrEmptyThrowWhenEmpty))]
+    [InlineData("")]
+    [InlineData("    ")]
+    [InlineData(null)]
+    public void NotNullOrEmptyThrowWhenEmpty(string? target)
+    {
+        Action action = () => DomainValidation.NotNullOrEmpty(target, "FieldName");
+        action.Should().Throw<EntityValidationException>().WithMessage("FieldName should not be null or empty");
+    }
+
+    [Fact(DisplayName = nameof(NotNullOrEmptyOk))]
+    public void NotNullOrEmptyOk()
+    {
+        var target = Faker.Random.String2(1, 100);
+        Action action = () => DomainValidation.NotNullOrEmpty(target, "FieldName");
+        action.Should().NotThrow();
+    }
+
+    [Theory(DisplayName = nameof(MinLengthThrowWhenLess))]
+    [MemberData(nameof(GenerateNamesSmallerThanLength), parameters: 10)]
+    public void MinLengthThrowWhenLess(string target, int minLength)
+    {
+        Action action = () => DomainValidation.MinLength(target, minLength, "FieldName");
+        action.Should().Throw<EntityValidationException>()
+            .WithMessage($"FieldName should be at least {minLength} characters long");
+    }
+    
+    public static IEnumerable<object[]> GenerateNamesSmallerThanLength(int testQuantity)
+    {
+        var faker = new Faker();
+        for (var i = 0; i < testQuantity; i++)
+        {
+            var example = faker.Commerce.ProductName();
+            var minLength = example.Length + (new Random()).Next(1, 20);
+            yield return new object[]{ example, minLength };
+        }
+    }
+    
+    
+    [Theory(DisplayName = nameof(MinLengthOk))]
+    [MemberData(nameof(GenerateNamesGreaterThanLength), parameters: 10)]
+    public void MinLengthOk(string target, int minLength)
+    {
+        Action action = () => DomainValidation.MinLength(target, minLength, "FieldName");
+        action.Should().NotThrow();
+    }
+    
+    public static IEnumerable<object[]> GenerateNamesGreaterThanLength(int testQuantity)
+    {
+        var faker = new Faker();
+        for (var i = 0; i < testQuantity; i++)
+        {
+            var example = faker.Commerce.ProductName();
+            var minLength = example.Length - (new Random()).Next(1, example.Length - 1);
+            yield return new object[]{ example, minLength };
+        }
     }
 }
